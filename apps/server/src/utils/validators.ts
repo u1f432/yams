ee3deea1e7a95ed905fe5cb78e613a5923bcb8c9
Jake Fulmine <jake@fulmine.xyz>
2023-09-29 12:09:30 +0200

    add optional but non nullable validator

---
+++ apps/server/src/utils/validators.ts
@@ -0,0 +1,18 @@
+import { IsOptional, ValidateIf, ValidationOptions } from 'class-validator'
+
+export function IsOptionalNonNullable(data?: {
+  nullable: boolean
+  validationOptions?: ValidationOptions
+}) {
+  const { nullable = false, validationOptions = undefined } = data || {}
+
+  if (nullable) {
+    // IsOptional allows null
+    return IsOptional(validationOptions)
+  }
+
+  return ValidateIf((ob: any, v: any) => {
+
+    return v !== undefined
+  }, validationOptions)
+}
